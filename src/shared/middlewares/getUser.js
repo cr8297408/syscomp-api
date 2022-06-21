2447b2ace2e5713f2e031fbfdff3f644ed97dabe
cr8297408 <cr8297408@gmail.com>
2022-06-21 17:50:31 -0500

    delete console log

--- src/shared/middlewares/getUser.js
+++ src/shared/middlewares/getUser.js
@@ -9,7 +9,6 @@ async function getUser(bearerHeader){
   if (token) {
       const decoded = await jwt.verify(token, config.JWT_SECRET);
       if (decoded) {
-        console.log(decoded)
         const user = await User.findByPk(decoded.dataToken.id)
         return user
       }
